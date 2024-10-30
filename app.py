350ad1feb8641843759de04f5a325c15e9211a07
pierswalker71 <60096809+pierswalker71@users.noreply.github.com>
2024-10-30 21:49:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
 
+# imports
 import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+from matplotlib.ticker import MaxNLocator
+import numpy as np
+
+from pydub import AudioSegment
+import librosa
+import librosa.display
+
+#import io
+#from IPython.display import Audio
+
+from scipy.signal import find_peaks
+from scipy.signal import butter, filtfilt
+from scipy.ndimage import gaussian_filter1d
+from scipy.stats import kurtosis
+from scipy.stats import entropy
+
+
+
 
 st.title("Cymbal analysis")
 st.write("This app loads cymbal audio files and presents their key features")
