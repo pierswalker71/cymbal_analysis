f7564b2e6b7926e5c6c14c15f07a0845e089da03
pierswalker71 <60096809+pierswalker71@users.noreply.github.com>
2024-12-16 13:10:52 +0000

    Update app.py

--- app.py
+++ app.py
@@ -275,9 +275,12 @@ metrics["peaks_per_band"] = {band:peak for band,peak in zip (freq_band_names,pea
 
 st.header("How long the cymbal rings for", divider="gray")
 
-st.write(f"It takes {decay_time_50pc:.1f}s for the sound to decay to 50% its initial maximum amplitude.")
-st.write(f"It takes {decay_time_10pc:.1f}s for the sound to decay to 10% its initial maximum amplitude.")
-st.markdown('<p style="color:blue; background-color:yellow; font-size:20px; font-weight:bold;">Eye-catching text!</p>', unsafe_allow_html=True)
+text = f"It takes {decay_time_50pc:.1f}s for the sound to decay to 50% its initial maximum amplitude."
+st.markdown('<p style="color:blue; background-color:yellow; font-size:20px; font-weight:bold;">{text}</p>', unsafe_allow_html=True)
+
+text = f"It takes {decay_time_10pc:.1f}s for the sound to decay to 10% its initial maximum amplitude."
+st.markdown('<p style="color:blue; background-color:yellow; font-size:20px; font-weight:bold;">{text}</p>', unsafe_allow_html=True)
+
 #--------------------------------------------------------------
 # Plot 1
 #--------------------------------------------------------------
