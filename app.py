a952b9325c2a1132a27e6fb2ff32985562518947
pierswalker71 <60096809+pierswalker71@users.noreply.github.com>
2024-12-16 16:45:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -456,10 +456,11 @@ with st.expander("Frequency bands", expanded=True):
     fig.update_layout(
         yaxis=dict(
             title='Proportion of energy',
-           title_font=dict(color="black"), tickfont=dict(color="black"),
+            title_font=dict(color="black"), tickfont=dict(color="black"),
             showline=True, linecolor='red', linewidth=1,
-            gridwidth=0.7, gridcolor='red',
-            range=[0, None]
+            gridwidth=0.7, gridcolor='rgba(255,153,153,0.7)',
+            range=[0, None],
+            dash='dot'
         ),
         yaxis2=dict(
             title='Number of peaks',
@@ -468,7 +469,7 @@ with st.expander("Frequency bands", expanded=True):
             gridwidth=0.7, gridcolor='lightgrey',
             overlaying='y',
             side='right',
-            range=[0, None],
+            range=[0, None]
         ),
         xaxis=dict(
             title='Frequency Band (Hz)',
