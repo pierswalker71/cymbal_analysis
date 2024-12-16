87f27b680267dbb17b904bab3d7cc849be59acd4
pierswalker71 <60096809+pierswalker71@users.noreply.github.com>
2024-12-16 16:29:56 +0000

    Update app.py

--- app.py
+++ app.py
@@ -357,8 +357,8 @@ st.header("The key frequencies", divider="gray")
 # Key stats
 
 st.markdown("**Most of the sound energy is concentrated in the following 5 bands:**")
-st.write(f"{indices[0]})
-st.write(f"{indices[1]})
+st.write(f"{indices[0]}")
+st.write(f"{indices[1]}")
 Top_5_bands = sorted(enumerate(normalized_band_energies), key=lambda x: x[1], reverse=True)[:5]
 # Extract indices and values
 indices, values = zip(*Top_5_bands)
