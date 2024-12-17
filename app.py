54ad03d45d54e084e393439c87079371161346cc
pierswalker71 <60096809+pierswalker71@users.noreply.github.com>
2024-12-17 15:33:37 +0000

    Update app.py

--- app.py
+++ app.py
@@ -931,7 +931,7 @@ with st.expander("Dominant frequencies over time",expanded=True):
         height=400, width=800
     )
 
-    st.write("how the peak frequencies evolve over time. The most prominent frequency peaks are identified within small, overlapping time windows, 
-    and this process is repeated continuously throughout the duration of the audio file")
+    st.write("how the peak frequencies evolve over time. The most prominent frequency peaks are identified within small, overlapping time windows, and this process is repeated continuously throughout the duration of the audio file")
     st.write("""Additionally, if any fundamental pitches are detected (i.e. the lowest frequency when harmonic frequencies are present as multiples of it), they are also displayed.""")
+
     st.plotly_chart(fig, use_container_width=True)
